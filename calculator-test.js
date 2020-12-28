605f77867dd21f41034ab3f847b1bfd715c9207b
John Curry <jtc3852@gmail.com>
2020-12-27 20:14:18 -0500

    Completed with basic Jasmine tests

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,20 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  let values = {
+    amount: 1000,
+    years: 15,
+    rate: 6,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('8.44');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  let values = {
+    amount: 15202,
+    years: 9,
+    rate: 3.25,
+  };
+  expect(calculateMonthlyPayment(values)).toEqual('162.54');
 });
 
-/// etc
--- calculator.js
+++ calculator.js
@@ -21,20 +21,35 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const loanAmountInitial = document.getElementById('loan-amount');
+  const yearsAmountInitial = document.getElementById('loan-years');
+  const rateAmountInitial = document.getElementById('loan-rate');
+  const defaultValues = {amount: 20000, years: 10, rate: 5.5};
+  loanAmountInitial.value = defaultValues.amount;
+  yearsAmountInitial.value = defaultValues.years;
+  rateAmountInitial.value = defaultValues.rate;
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let currentValues = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(currentValues));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let monthlyRate = (values.rate /100) / 12;
+  let n = Math.floor(values.years *12);
+  return ((values.amount*monthlyRate)/(1-(1+monthlyRate)**(-n))).toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  let monthlyPaymentValue = document.getElementById('monthly-payment');
+  monthlyPaymentValue.textContent = `$${monthly}`;
 }
